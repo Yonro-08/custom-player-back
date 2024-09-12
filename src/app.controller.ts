fe8ba16df3bb0d2e282330d8abd9b7ffd52d0800
Yonro <143833565+Yonro-08@users.noreply.github.com>
2024-09-13 02:08:06 +0500

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,33 @@
+import {
+	Controller,
+	Post,
+	UploadedFile,
+	UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import * as path from 'path';
+
+@Controller('/api/upload')
+export class AppController {
+	constructor(private readonly appService: AppService) {}
+
+	@Post()
+	@UseInterceptors(
+		FileInterceptor('video', {
+			storage: multer.diskStorage({
+				destination: './uploads/original',
+				filename: (req, file, cb) => {
+					const uniqueSuffix =
+						Date.now() + '-' + Math.round(Math.random() * 1e9);
+					const fileExtName = path.extname(file.originalname);
+					cb(null, `${uniqueSuffix}${fileExtName}`);
+				},
+			}),
+		})
+	)
+	uploadVideo(@UploadedFile() file: Express.Multer.File) {
+		return this.appService.processVideo(file);
+	}
+}
---
+++ src/app.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { IResolution, RESOLUTIONS } from './resolution.data';
+import * as ffmpeg from 'fluent-ffmpeg';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+@Injectable()
+export class AppService {
+	private _outputDir = './uploads';
+	constructor() {
+		ffmpeg.setFfmpegPath(ffmpegPath);
+	}
+
+	async processVideo(file: Express.Multer.File): Promise<{ message: string }> {
+		const originalPath = path.join(this._outputDir, 'original', file.filename);
+
+		await Promise.all(
+			RESOLUTIONS.map(resolution => {
+				return this._convertVideo(originalPath, resolution, file.filename);
+			})
+		);
+
+		return { message: 'Video processed successfully' };
+	}
+
+	private async _convertVideo(
+		inputPath: string,
+		resolution: IResolution,
+		fileName: string
+	) {
+		const outputDir = path.join(this._outputDir, resolution.name);
+		if (!fs.existsSync(outputDir)) {
+			fs.mkdirSync(outputDir, { recursive: true });
+		}
+
+		const outputPath = path.join(outputDir, fileName);
+
+		return new Promise<void>((resolve, reject) => {
+			ffmpeg(inputPath)
+				.size(`${resolution.width}x${resolution.height}`)
+				.output(outputPath)
+				.on('end', () => resolve())
+				.on('error', err => reject(err))
+				.run();
+		});
+	}
+}
